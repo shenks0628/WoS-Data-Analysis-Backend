76365d9cca60be0deb41986eda019e48cd837648
joecarol <a0900612007@gmail.com>
2024-07-15 21:28:20 +0800

    sc

--- fieldAnalysis.py
+++ fieldAnalysis.py
@@ -1,8 +1,9 @@
 import os
 import glob
 import requests
-
-def fieldAnalysisyear(files, filesToAnalyze, start, end):
+def fieldAnalysis(files, filesToAnalyze, start, end):
+    count = 0
+    conditionCount = 0
     fieldAnalysis_count = dict()
     for file in files:
         fileName = file.get('name')
@@ -15,33 +16,41 @@ def fieldAnalysisyear(files, filesToAnalyze, start, end):
         fieldAnalysis = ""
         insideSC = False
         for line in content.split('\n'):
-            if line.startswith("SC "):
+            if line.startswith("TI "):
+                count += 1
+            elif line.startswith("SC "):
                 insideSC = True
                 fieldAnalysis += line[3:].strip()
+                fieldAnalysis += ';'
             elif line.startswith("   ") and insideSC:
                 fieldAnalysis += line[3:].strip()
+                fieldAnalysis += ';'
             elif line.startswith("PY "):
                 year = int(line[3:].strip())
-                if year >= start and year <= end:
-                    if fieldAnalysis != "":
-                        fieldAnalysis = fieldAnalysis.split(';')
-                        for word in fieldAnalysis:
-                            word = word.strip().lower()
-                            if fieldAnalysis_count.get(word, False):
-                                fieldAnalysis_count[word] += 1
-                            else:
-                                fieldAnalysis_count[word] = 1
-                fieldAnalysis = ""
                 insideSC = False
+            else:
+                if insideSC:
+                    if year >= start and year <= end:
+                        if fieldAnalysis != "":
+                            conditionCount += 1
+                            fieldAnalysis = fieldAnalysis.split(';')
+                            for word in fieldAnalysis:
+                                word = word.strip().lower()
+                                if fieldAnalysis_count.get(word, False):
+                                    fieldAnalysis_count[word] += 1
+                                else:
+                                    fieldAnalysis_count[word] = 1
+                    fieldAnalysis = ""
+                    insideSC = False
     sorted_fieldAnalysis = sorted(fieldAnalysis_count.items(), key=lambda x: x[1], reverse=True)
     results = []
     cnt = 0
     for fieldAnalysis in sorted_fieldAnalysis:
         results.append({
-            'author': fieldAnalysis[0],
+            'fieldAnalysis': fieldAnalysis[0],
             'count': fieldAnalysis[1]
         })
         cnt += 1
         if cnt >= 100:
             break
-    return results
+    return count, conditionCount, results
