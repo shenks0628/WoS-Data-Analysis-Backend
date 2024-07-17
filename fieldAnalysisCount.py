c38e6f7ad019669bd40e765726984bc949e8029c
DHugo324 <116426935+DHugo324@users.noreply.github.com>
2024-07-17 20:33:54 +0800

    add fieldOccurence

--- fieldAnalysisCount.py
+++ fieldAnalysisCount.py
@@ -51,3 +51,50 @@ def fieldAnalysisyear(files, filesToAnalyze, start, end):
         if cnt >= 100:
             break
     return count, conditionCount, results
+
+def fieldOccurence(files, filesToAnalyze, threshold):
+    field_count = dict()
+    titleCount = 0
+    for file in files:
+        fileName = file.get('name')
+        if fileName not in filesToAnalyze:
+            continue
+        fileURL = file.get('url')
+        response = requests.get(fileURL)
+        response.encoding = 'utf-8'
+        content = response.text
+        field = ""
+        insideSC = False
+        for line in content.split('\n'):
+            if line.startswith("TI "):
+                titleCount += 1
+            elif line.startswith("SC "):
+                insideSC = True
+                field += line[3:].strip()
+            elif line.startswith("   ") and insideSC:
+                field += line[3:].strip()
+            else:
+                if field != "":
+                    field = field.split(';')
+                    for word in field:
+                        word = word.strip().lower()
+                        if field_count.get(word, False):
+                            field_count[word] += 1
+                        else:
+                            field_count[word] = 1
+                field = ""
+                insideSC = False
+    sorted_fields = sorted(field_count.items(), key=lambda x: x[1], reverse=True)
+    results = []
+    cnt = 0
+    for field in sorted_fields:
+        if field[1] < threshold:
+            break
+        results.append({
+            'field': field[0],
+            'count': field[1]
+        })
+        cnt += 1
+        if cnt >= 100:
+            break
+    return titleCount, results
